c543deb2ca28d11fb924d63ce63663768e805ce8
rgdevment <mhidalgog@outlook.com>
2024-04-18 20:01:12 -0400

    feat: initial base project

---
+++ src/test/java/cl/tica/portfolio/recipeapi/RecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.tica.portfolio.recipeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
