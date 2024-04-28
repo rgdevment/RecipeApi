428e5e23abf4d8e0366d5debfc599a1ee57b8b14
rgdevment <mhidalgog@outlook.com>
2024-04-28 15:41:11 -0400

    revert: remove enable async

--- src/main/java/cl/tica/portfolio/recipeapi/configs/AppConfig.java
+++ src/main/java/cl/tica/portfolio/recipeapi/configs/AppConfig.java
@@ -4,6 +4,5 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@EnableAsync
 public class AppConfig {
 }
