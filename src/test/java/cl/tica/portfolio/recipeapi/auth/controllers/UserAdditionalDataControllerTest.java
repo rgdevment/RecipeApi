711f08b31a79ec1ec3c94fed5bbf25edf2673c31
rgdevment <mhidalgog@outlook.com>
2024-05-03 01:20:12 -0400

    feat: replace UserPrincipalMock by @WithMockUser

--- src/test/java/cl/tica/portfolio/recipeapi/auth/controllers/UserAdditionalDataControllerTest.java
+++ src/test/java/cl/tica/portfolio/recipeapi/auth/controllers/UserAdditionalDataControllerTest.java
@@ -7,7 +7,6 @@ import cl.tica.portfolio.recipeapi.auth.entities.UserTestStub;
 import cl.tica.portfolio.recipeapi.auth.enums.GenderType;
 import cl.tica.portfolio.recipeapi.auth.security.SecurityConfig;
 import cl.tica.portfolio.recipeapi.auth.services.UserAdditionalDataService;
-import cl.tica.portfolio.recipeapi.auth.utils.UserPrincipalMock;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.datafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,13 +16,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(SecurityConfig.class)
@@ -43,9 +44,10 @@ class UserAdditionalDataControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "testUser")
     void addUserAdditionalData() throws Exception {
         Faker faker = new Faker();
-        User userFake = UserTestStub.random();
+        User userFake = UserTestStub.create("testUser", "", "");
         UserAdditionalData userAdditionalData = userFake.getUserData();
         userAdditionalData.setName(faker.name().firstName());
         userAdditionalData.setLastname(faker.name().lastName());
@@ -57,12 +59,76 @@ class UserAdditionalDataControllerTest {
         when(service.findUserByUsername(userFake.getUsername())).thenReturn(userFake);
 
         mockMvc.perform(MockMvcRequestBuilders.put("/v1/user/additional-data")
-                        .with(user(new UserPrincipalMock(userFake.getUsername())))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk());
 
         verify(service, times(1)).findUserByUsername(userFake.getUsername());
         verify(service, times(1)).updateUserData(userFake);
+    }
+
+    @Test
+    void addUserAdditionalDataWithOutAuthenticate() throws Exception {
+        Faker faker = new Faker();
+        User userFake = UserTestStub.random();
+        UserAdditionalData userAdditionalData = userFake.getUserData();
+        userAdditionalData.setName(faker.name().firstName());
+        userAdditionalData.setLastname(faker.name().lastName());
+        userAdditionalData.setGender(GenderType.OTHER);
+
+        AdditionalDataRequest request = new AdditionalDataRequest(userAdditionalData.getName(),
+                userAdditionalData.getLastname(), userAdditionalData.getGender().name());
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/v1/user/additional-data")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isUnauthorized());
+
+        verify(service, never()).findUserByUsername(any());
+        verify(service, never()).updateUserData(any());
+    }
+
+    @Test
+    @WithMockUser(username = "testUser")
+    void withInvalidUpdateGender() throws Exception {
+        Faker faker = new Faker();
+        User userFake = UserTestStub.create("testUser", "", "");
+        UserAdditionalData userAdditionalData = userFake.getUserData();
+        userAdditionalData.setName(faker.name().firstName());
+        userAdditionalData.setLastname(faker.name().lastName());
+
+        AdditionalDataRequest request = new AdditionalDataRequest(userAdditionalData.getName(),
+                userAdditionalData.getLastname(), "INVALID");
+
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/v1/user/additional-data")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+
+        verify(service, never()).findUserByUsername(any());
+        verify(service, never()).updateUserData(any());
+    }
+
+    @Test
+    @WithMockUser(username = "testUser")
+    void withInvalidUpdateGenderEmpty() throws Exception {
+        Faker faker = new Faker();
+        User userFake = UserTestStub.create("testUser", "", "");
+        UserAdditionalData userAdditionalData = userFake.getUserData();
+        userAdditionalData.setName(faker.name().firstName());
+        userAdditionalData.setLastname(faker.name().lastName());
+
+        AdditionalDataRequest request = new AdditionalDataRequest(userAdditionalData.getName(),
+                userAdditionalData.getLastname(), null);
+
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/v1/user/additional-data")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+
+        verify(service, never()).findUserByUsername(any());
+        verify(service, never()).updateUserData(any());
     }
 }
